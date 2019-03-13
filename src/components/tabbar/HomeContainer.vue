1405d22deea2591673c4523fae56359bf4e5b3e3
zaecy <1319055005@qq.com>
2019-03-13 10:57:56 +0800

    打卡

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -14,24 +14,24 @@
 		<!-- 九宫格到六宫格改造 -->
 		<div class="mui-content">
 	        <ul class="mui-table-view mui-grid-view mui-grid-9" >
-	            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+	            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="/home/newslist">
 		            	<img src="../../images/menu1.png" alt="">
-	                    <div class="mui-media-body">新闻资讯</div></a></li>
-	            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+	                    <div class="mui-media-body">新闻资讯</div></router-link></li>
+	            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="">
 	                    <img src="../../images/menu2.png" alt="">
-	                    <div class="mui-media-body">图片分享</div></a></li>
-	            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+	                    <div class="mui-media-body">图片分享</div></router-link></li>
+	            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="">
 	                    <img src="../../images/menu3.png" alt="">
-	                    <div class="mui-media-body">商品购买</div></a></li>
-	            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+	                    <div class="mui-media-body">商品购买</div></router-link></li>
+	            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="">
 	                    <img src="../../images/menu4.png" alt="">
-	                    <div class="mui-media-body">留言反馈</div></a></li>
-	            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+	                    <div class="mui-media-body">留言反馈</div></router-link></li>
+	            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="">
 	                    <img src="../../images/menu5.png" alt="">
-	                    <div class="mui-media-body">视频专区</div></a></li>
-	            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+	                    <div class="mui-media-body">视频专区</div></router-link></li>
+	            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="">
 	                    <img src="../../images/menu6.png" alt="">
-	                    <div class="mui-media-body">联系我们</div></a></li>
+	                    <div class="mui-media-body">联系我们</div></router-link></li>
 	            
 	        </ul> 
 		</div>
@@ -52,7 +52,7 @@
 			this.getLunbotu()
 		},
 		methods:{
-			getLunbotu(){  //获取轮播图数据的方法
+			getLunbotu(){  //获取轮播图数据的方法  http://www.liulongbin.top:3005/api/getlunbo
 				this.$http.get('http://www.liulongbin.top:3005/api/getlunbo').then(result => {
 					// console.log(result.body);
 					if(result.body.status === 0){
--- src/main.js
+++ src/main.js
@@ -6,10 +6,20 @@ import VueRouter from 'vue-router'
 //1.2 安装路由
 Vue.use(VueRouter)
 
+
+//导入格式化时间的插件
+import moment from 'moment'
+//定义全局过滤器
+Vue.filter('dateFormat',function(dataStr,pattern="YYYY-MM-DD HH:mm:ss"){
+	return moment(dataStr).format(pattern)
+})
+
 //2.1 导入 vue-resource
 import VueResource from 'vue-resource'
-//2.2 安装 vue0resource
+//2.2 安装 vue-resource
 Vue.use(VueResource)
+//设置请求的根路径
+Vue.http.options.root = 'http://www.liulongbin.top:3005/';
 
 //导入mui的样式
 import './lib/mui/css/mui.min.css'
@@ -17,10 +27,11 @@ import './lib/mui/css/icons-extra.css'
 
 
 //按需导入Mint-UI中的组件
-import { Header,Swipe, SwipeItem } from 'mint-ui'
+import { Header,Swipe, SwipeItem, Button } from 'mint-ui'
 Vue.component(Header.name, Header)
 Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
+Vue.component(Button.name, Button);
 
 
 
